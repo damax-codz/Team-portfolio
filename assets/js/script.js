acc44b242154ca881b6da027539d5e2de6d27f49
damax-codz <kingowu878@gmail.com>
2022-03-21 17:39:15 +0100

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,12 @@ else{
 goToTeam=()=>{
   
 }
+console.log('Hello World!');
+
+
+const all = (arr,fn) => arr.every(fn);
+
+console.log(typeof Number)// true
+console.log(all(["tu", 2, 3],x=>x>10)); // true
+
+
